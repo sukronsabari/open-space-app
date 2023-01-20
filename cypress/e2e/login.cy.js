274b2447c62ad70e610cef746a11b1f5286be86c
sukronsabari11 <sukronsabari11@gmail.com>
2023-01-20 21:43:41 +0800

    add ci action

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,12 +7,13 @@
  *   - should display homepage when username and password are correct
  */
 
-describe('Login specs', () => {
+describe('Login spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
   });
 
   it('should display login page correctly', () => {
+    // memverifikasi elemen yang harus tampak pada halaman login
     cy.get('input[placeholder="Username"]').should('be.visible');
     cy.get('input[placeholder="Password"]').should('be.visible');
     cy.get('button')
@@ -21,46 +22,66 @@ describe('Login specs', () => {
   });
 
   it('should display alert when username is empty', () => {
+    // klik tombol login tanpa mengisi username
     cy.get('button')
       .contains(/^Login$/)
       .click();
 
+    // memverifikasi window.alert untuk menampilkan pesan dari API
     cy.on('window:alert', (str) => {
       expect(str).to.equal('"id" is not allowed to be empty');
     });
   });
 
   it('should display alert when password is empty', () => {
+    // mengisi username
     cy.get('input[placeholder="Username"]').type('testuser');
 
+    // klik tombol login tanpa mengisi password
     cy.get('button')
       .contains(/^Login$/)
       .click();
 
+    // memverifikasi window.alert untuk menampilkan pesan dari API
     cy.on('window:alert', (str) => {
       expect(str).to.equal('"password" is not allowed to be empty');
     });
   });
 
   it('should display alert when username and password are wrong', () => {
-    cy.get('input[placeholder="Username"').type('akunx');
-    cy.get('input[placeholder="Password"').type('123456x');
+    // mengisi username
+    cy.get('input[placeholder="Username"]').type('testuser');
+
+    // mengisi password yang salah
+    cy.get('input[placeholder="Password"]').type('wrong_password');
+
+    // menekan tombol Login
     cy.get('button')
       .contains(/^Login$/)
       .click();
 
+    // memverifikasi window.alert untuk menampilkan pesan dari API
     cy.on('window:alert', (str) => {
       expect(str).to.equal('User ID or password is wrong');
     });
   });
 
   it('should display homepage when username and password are correct', () => {
-    cy.get('input[placeholder="Username"').type('testuser');
-    cy.get('input[placeholder="Password"').type('test123456');
-    cy.get('button').contains('Login').click();
+    // mengisi username
+    cy.get('input[placeholder="Username"]').type('testuser');
+
+    // mengisi password
+    cy.get('input[placeholder="Password"]').type('test123456');
+
+    // menekan tombol Login
+    cy.get('button')
+      .contains(/^Login$/)
+      .click();
 
     // memverifikasi bahwa elemen yang berada di homepage ditampilkan
-    cy.get('nav').contains(/^Home/).should('be.visible');
+    cy.get('nav')
+      .contains(/^Home$/)
+      .should('be.visible');
     cy.get('button').contains('Sign out').should('be.visible');
   });
 });
